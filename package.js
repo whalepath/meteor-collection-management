33b8142625cc348c2e00865a1baf969fe895bc07
Patrick Moore <patmoore@farreach.es>
2014-04-28 14:50:41 -0700

    debugger

--- package.js
+++ package.js
@@ -4,6 +4,7 @@ Package.describe({
 
 Package.on_use(function(api) {
   //api.use('service-configuration', ['client', 'server']);
+  debugger;
 
   api.export('DbObjectType');
   api.export('ManagerType');
