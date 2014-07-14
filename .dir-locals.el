667247276c8ae67fc418ef398376e5169fda621e
Daniel Radetsky <dradetsky@gmail.com>
2014-07-14 09:31:14 -0700

    Add correct indenting to emacs project settings.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;; This file sets emacs variables for the project/directory
 
 ; indent 4 spaces
-((js2-mode . ((js2-basic-offset . 4))))
+((js2-mode . ((js2-basic-offset . 4)
+              ; allow indenting to correct level
+              (js2-bounce-indent-p . t))))
