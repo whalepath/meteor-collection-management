1a77b716867de5d14315eb1950fff33f4144bdfd
Daniel Radetsky <dradetsky@gmail.com>
2014-07-22 15:49:31 -0700

    Move phantomjs workaround to new file, client only

--- package.js
+++ package.js
@@ -13,6 +13,7 @@ Package.on_use(function(api) {
   api.add_files('lib/enums.js', ['client', 'server']);
   api.add_files('lib/dbobject.js', ['client', 'server']);
   api.add_files('lib/manager.js', ['client', 'server']);
+  api.add_files('lib/phantomjsCompat.js', 'client');
   api.add_files('client/manager.js', ['client' ]);
   api.add_files('server/manager.js', 'server');
 });
