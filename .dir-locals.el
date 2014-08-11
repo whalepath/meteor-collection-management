bd31efe76105df261b91cba6120159b499352dec
Daniel Radetsky <dradetsky@gmail.com>
2014-08-11 10:01:20 -0700

    Add more project settings for emacs.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,7 @@
 ; indent 4 spaces
 ((js2-mode . ((js2-basic-offset . 4)
               ; allow indenting to correct level
-              (js2-bounce-indent-p . t))))
+              (js2-bounce-indent-p . t)
+              ; wrap lines to 100 chars
+              (fill-column . 100)))
+ (html-mode . ((sgml-basic-offset . 4))))
