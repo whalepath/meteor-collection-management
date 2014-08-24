8b6175684bf5d31db2d0468c3cf4d4fe4236dc4a
Daniel Radetsky <dradetsky@gmail.com>
2014-08-24 15:44:15 -0700

    Improve emacs project vars

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,11 @@
 ;; This file sets emacs variables for the project/directory
 
-; indent 4 spaces
-((js2-mode . ((js2-basic-offset . 4)
+; nil means applies to every mode; wrap lines to 100 chars
+((nil . ((fill-column . 100)
+         ; when using fiplr fuzzy project find, ignore these
+         (fiplr-ignored-globs . ((directories (".meteor" ".build"))
+                                 (files ("*.jpg" ".png"))))))
+ (js2-mode . ((js2-basic-offset . 4)
               ; allow indenting to correct level
               (js2-bounce-indent-p . t)
               ; wrap lines to 100 chars
