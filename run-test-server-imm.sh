e407046741a5d1696568358871a9cb2cb5389287
Patrick Moore <patmoore@farreach.es>
2014-09-01 12:51:27 -0700

    scripts

---
+++ run-test-server-imm.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# meteor help test-packages
+cat<<EOM
+
+
+Open the test dashboard in your browser to run the tests and see the results.
+
+        http://localhost:54321
+
+(This script will not end by itself.)
+EOM
+
+NODE_OPTIONS=--debug-brk meteor -p 54321 test-packages ./
