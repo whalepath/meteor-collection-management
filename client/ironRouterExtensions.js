e2a614fd7259bef597e17fd657caed5a54867b34
Patrick Moore <patmoore@farreach.es>
2014-09-05 11:26:35 -0700

    comments and check to see if the template functions are being called by the iron:router code.
    Also comment that the initialization code present is being run too early, so for now that init code needs to be copied to whalepath-server

--- client/ironRouterExtensions.js
+++ client/ironRouterExtensions.js
@@ -1,8 +1,24 @@
 // TODO: put in a pull request to iron-router
 // this works but can we use a global hook that for a given route does a universal lookup.
 if ( Router != null) {
+    /**
+     * A standard data() that will be called by the Router code to get the template's data.
+     * This function will be used by the Router code.
+     *
+     *
+     *
+     * @returns {{}}
+     */
     Template.prototype.data = function() {
-        var initializeData = this.route.options.initializeData;
+        var initializeData;
+        if ( this.route != null) {
+            //we are being called by the iron:router code
+            initializeData = this.route.options.initializeData;
+        } else {
+            // TODO : how to get the template instance?
+            initializeData = null;
+        }
+
         if ( initializeData ) {
             var initialData;
             if ( typeof initializeData ==='function' ) {
@@ -35,7 +51,14 @@ if ( Router != null) {
         }
     }
     Template.prototype.waitOn = function() {
-        var initializeData = this.route.options.initializeData;
+        var initializeData;
+        if ( this.route != null) {
+            //we are being called by the iron:router code
+            initializeData = this.route.options.initializeData;
+        } else {
+            // TODO : how to get the template instance?
+            initializeData = null;
+        }
         if ( initializeData ) {
             var initialData;
             if ( typeof initializeData ==='function' ) {
@@ -53,13 +76,17 @@ if ( Router != null) {
             return result;
         }
     }
-    // if a route does not have a waitOn function borrow from the Template's waitOn.
+
+    // TODO: This does not work because no routes are defined at this moment
+    // need to see if we can hook the route creation.
     _.each(Router.routes, function (route) {
         var templateName = route.router.convertTemplateName(route.name);
         var template = Template[templateName];
         // not all routes have templates...
         if ( template ) {
             _.each(['waitOn', 'data', 'initializeData'], function (action) {
+                // if a route does not have a function use the Template's function
+                // maybe in future merge Router.xx() and Template.xx() so that the results are combined?
                 if (typeof route.options[action] === 'undefined') {
                     route.options[action] = template[action];
                 }
