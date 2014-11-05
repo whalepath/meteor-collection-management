091c2e219dc1fa518d1509960f641dad978f81ff
Daniel Radetsky <dradetsky@gmail.com>
2014-11-05 15:51:57 -0800

    Revert "for meteor 1.0"
    This reverts commit 253bc7c9fa7f2ee04209b935e00d561c57e0db43.

--- client/ironRouterExtensions.js
+++ client/ironRouterExtensions.js
@@ -172,7 +172,7 @@ if ( Router != null) {
         // TODO: This does not work because no routes are defined at this moment
         // need to see if we can hook the route creation.
         _.each(Router.routes, function (route) {
-            var templateName = route.options.template || route.router.toTemplateName(route.getName());
+            var templateName = route.options.template || route.router.convertTemplateName(route.name);
             var template = Template[templateName];
             // not all routes have templates...
             if (template) {
