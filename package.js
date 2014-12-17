8fe9a78e8a61732548986f76127e56de00b7175c
Daniel Radetsky <dradetsky@gmail.com>
2014-12-17 15:41:20 -0800

    non-breaking api change to upsert, revision save

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.2.0",
+    version: "1.3.0",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
