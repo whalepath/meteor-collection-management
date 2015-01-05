e1585d13154a1f2cced67977e9648a6f22c6d279
Patrick Moore <patmoore@farreach.es>
2015-01-04 18:12:02 -0800

    add findRequiredOneBy

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.5.0",
+    version: "1.5.1",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
