a52d8e744a1bbe67abcda78edcb63761306dee91
Patrick Moore <patmoore@farreach.es>
2015-02-24 13:07:42 -0800

    Fix https://github.com/whalepath/whalepath-server/issues/515 https://github.com/whalepath/whalepath-server/issues/516 - turn on use of waitOn for navigating the site. This means that every navigation must wait for the data initializeData() to be available.
    I have been using this code for a few weeks so I think there are no problems with this new behavior.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.5.7",
+    version: "1.6.0",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
