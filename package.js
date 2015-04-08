501cf9b5a8523b31a288439dc2dee79db518c695
Patrick Moore <patmoore@farreach.es>
2015-04-07 21:56:56 -0700

    dbObject now has: updateOneById, updateOneBy, updateAllBy, updateBy()

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.6.11",
+    version: "1.6.12",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
