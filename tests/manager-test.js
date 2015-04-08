0ffbb127642110ef231c10487cf40a2dd1a59f54
Patrick Moore <patmoore@farreach.es>
2015-04-08 11:04:47 -0700

    fix test

--- tests/manager-test.js
+++ tests/manager-test.js
@@ -40,22 +40,28 @@ Tinytest.add('Meteor Collection Management - manager - simple', function(test) {
             }
         }
     });
-    TestManagerType.prototype.createMeteorCallMethod = function(definition, definitionName) {
-        var fn = definition.method;
-        var thatManager = this.thatManager;
-        test.equal(_.isFunction(fn), true, "no method:"+definitionName);
-        var actual = fn.call(thatManager);
-        calledMethods[actual] = true;
-        test.equal(definitionName, actual, "Bad method definition");
-    }
-    TestManagerType.prototype.createPublication = function(definition, definitionName) {
-        var fn = definition.cursor;
-        var thatManager = this.thatManager;
-        test.equal(_.isFunction(fn), true, "no cursor:"+definitionName);
-        var actual = fn.call(thatManager);
-        calledMethods[actual] = true;
-        test.equal(definitionName, actual, "Bad cursor definition");
-    }
+    Object.defineProperties(TestManagerType.prototype, {
+        createMeteorCallMethod: {
+            value: function (definition, definitionName) {
+                var fn = definition.method;
+                var thatManager = this.thatManager;
+                test.equal(_.isFunction(fn), true, "no method:" + definitionName);
+                var actual = fn.call(thatManager);
+                calledMethods[actual] = true;
+                test.equal(definitionName, actual, "Bad method definition");
+            }
+        },
+        createPublication: {
+            value: function (definition, definitionName) {
+                var fn = definition.cursor;
+                var thatManager = this.thatManager;
+                test.equal(_.isFunction(fn), true, "no cursor:" + definitionName);
+                var actual = fn.call(thatManager);
+                calledMethods[actual] = true;
+                test.equal(definitionName, actual, "Bad cursor definition");
+            }
+        }
+    });
 
     var TestManager = new TestManagerType();
     test.equal(_.keys(calledMethods).length, 6, "should have called 3 methods and 3 cursors but only called: "+ _.keys(calledMethods));
