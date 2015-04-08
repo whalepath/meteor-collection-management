cb094c9fab60f2790ca617fd47e19a7dadb10ac0
Patrick Moore <patmoore@farreach.es>
2015-04-08 10:23:18 -0700

    fix to enums.ninMongoDb()

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.6.13",
+    version: "1.6.14",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
