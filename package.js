22661101ef42101c0307d97e83c0a6eb36f46bb6
Patrick Moore <patmoore@farreach.es>
2015-04-10 13:48:17 -0700

    We were experiencing forks of revisions. We would have duplicate challenge Entries that were identical but have different seqIds. We did not definitively determine how this happened. However, this commit fixes potential causes of the issue.
    1. when a revision was saved in previous code, the old revision head is returned. So a code path that makes some changes, does a save, makes some more changes, and then does another save would cause a fork. This commit changes the return from _save() to return the object from _revisionSave()
    2. by default, forks were allowed so a _save({revision:true}) would allow a fork since the reviseHeadOnly parameter was not true. The default has now been changed, _save({revision:true}) by default does not allow forking (NOTE: currently, allowForks is hardcoded to false to see if this commit removes the entry forking)
    3. Note: if _revisionSave() or _save({revision:true}) fails a undefined will be returned. Callers should handle race conditions that would result in the revisionSave failing.
    
    Fix https://github.com/whalepath/whalepath-server/issues/600
    Fix https://github.com/whalepath/whalepath-server/issues/526

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.6.17",
+    version: "1.7.0",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
