b9d283f31a8409578da45f41b2927c67b8687788
Daniel Radetsky <dradetsky@gmail.com>
2015-04-09 17:35:25 -0700

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.6.16",
+    version: "1.6.17",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
