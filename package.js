716cadc7600e85a17fa9d2c2ca4fa822de308787
Patrick Moore <patmoore@farreach.es>
2015-06-04 09:43:15 -0700

    better error message when insert/update fails

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.8.2",
+    version: "1.8.3",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
