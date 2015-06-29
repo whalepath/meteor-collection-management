da87623d1fafcff53c38dc82a78429f30a3c82cd
Patrick Moore <patmoore@farreach.es>
2015-06-28 17:10:49 -0700

    up version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.8.9",
+    version: "1.8.10",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
