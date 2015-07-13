f5c21e97b2f7fea5752b79b31e559f3e52d8d353
Patrick Moore <patmoore@farreach.es>
2015-07-13 16:51:02 -0700

    allow reading setting from private assets.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.8.10",
+    version: "1.8.11",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
