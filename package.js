a76bcb33000ba6afa40eba217d6c80ba49a2a947
Patrick Moore <patmoore@farreach.es>
2015-07-21 08:46:00 -0700

    fix enums toDbArray: so to handle empty arrays passed.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.8.11",
+    version: "1.8.12",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
