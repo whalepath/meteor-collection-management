fc5f80a1f4778cb792e9bdfbc268346dcededcd5
Patrick Moore <patmoore@farreach.es>
2015-09-16 12:25:53 -0700

    bug fix. in findBy functions: query options were dropped.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor Collection Management",
-    version: "1.9.0",
+    version: "1.9.1",
     git: "https://github.com/whalepath/meteor-collection-management.git"
 });
 
